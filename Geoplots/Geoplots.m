3bd46e661836be4d97995ab8fa5f936461807aa4
TitusUni <161594474+TitusUni@users.noreply.github.com>
2024-03-04 14:15:14 +0100

    Add files via upload

--- Geoplots/Geoplots.m
+++ Geoplots/Geoplots.m
@@ -3,13 +3,15 @@ clc, clear, close, dbstop if error;
 buff = 0.5;
 colors = ["#0072BD","#D95319","#EDB120","#7E2F8E","#77AC30"];
 
-fotos = [""];
+% names of the pictures
+fotos = ["See_Sommer.jpg"];
 
 for j = 1:length(fotos)
-    dateiname = ['Ergebnis-', char(fotos(j)), '.txt'];
-    
-    
-    Ergebnislondon = importfile(dateiname, [1, Inf]);
+    %% Load Pictures
+
+    dateiname = ['Geoplot-', char(fotos(j)), '.txt'];
+   
+    Data = importfile(['Input/',dateiname], [1, Inf]);
     
     KoordinatenX = zeros([5,5]);
     KoordinatenY = zeros([5,5]);
@@ -17,20 +19,20 @@ for j = 1:length(fotos)
     Celle = zeros([5,1]);
     
     for i = 1:5
-        str1 = char(Ergebnislondon(i*4-3));
+        str1 = char(Data(i*4-3));
         doub1 = str2double(strsplit(str1(2:end-1),' '));
-        str2 = char(Ergebnislondon(i*4-2));
+        str2 = char(Data(i*4-2));
         doub2 = str2double(strsplit(str2(2:end-1),' '));
         KoordinatenX(i,1:4)=doub1(1:4);
         KoordinatenY(i,1:4)=doub2(1:4);
-        Prop(i) = str2double(char(Ergebnislondon(i*4-1)))*100;
-        Celle(i) = str2double(char(Ergebnislondon(i*4)));
+        Prop(i) = str2double(char(Data(i*4-1)))*100;
+        Celle(i) = str2double(char(Data(i*4)));
     end
     
     KoordinatenX(:,5) = KoordinatenX(:,1);
     KoordinatenY(:,5) = KoordinatenY(:,1);
 
-    fprintf('Foto %d eingeladen \n',j)
+    fprintf('Picture %d loaded \n',j)
     
     %% Plot
     figure('Visible','off');
@@ -44,7 +46,7 @@ for j = 1:length(fotos)
     geolimits([-90,90],[-180,180]);
     geobasemap landcover
     legend("rank 1","rank 2","rank 3","rank 4","rank 5","Location","southeast");
-    fprintf('- Uebersicht fertig \n');
+    fprintf('- Overview done \n');
     %%{
     for i =1:5
         buffX=(max(KoordinatenX(i,:))-min(KoordinatenX(i,:)))*buff;
@@ -55,24 +57,19 @@ for j = 1:length(fotos)
         geolimits([min(KoordinatenX(i,:))-buffX,max(KoordinatenX(i,:)+buffX)],[min(KoordinatenY(i,:))-buffY,max(KoordinatenY(i,:)+buffY)])
         geobasemap streets
         title(sprintf('Cell %d with %.2f %%', Celle(i),Prop(i)))
-        fprintf('- Plot %d von 5 fertig (%d) \n',i,j)
+        fprintf('- Plot %d from 5 done (%d) \n',i,j)
     end
     %}
-    exportgraphics(gcf,[dateiname(1:end-8),'.jpg'],'Resolution',300);
-    fprintf('Foto %d geplottet \n',j)
+    exportgraphics(gcf,['Output/',dateiname(1:end-8),'.jpg'],'Resolution',300);
+    fprintf('Picture %d plotted \n',j)
 
     clc;
     close all;
-    % Liste der Variablennamen, die behalten werden sollen
+
+    % Empty working memory
     behalteVariablen = {'fotos', 'colors', 'buff','j'};
-    
-    % Hol dir eine Liste aller aktuellen Variablennamen im Workspace
     alleVariablen = evalin('base', 'who');
-    
-    % Finde die zu löschenden Variablennamen
     zuLoeschendeVariablen = setdiff(alleVariablen, behalteVariablen);
-    
-    % Lösche die unerwünschten Variablen
     for i = 1:length(zuLoeschendeVariablen)
         evalin('base', ['clear ', zuLoeschendeVariablen{i}]);
     end
--- Geoplots/README.md
+++ Geoplots/README.md
@@ -1,1 +1,14 @@
-# Hier Bedienung einfügen 
+## Implementation of the Geoplots
+
+This repository contains scripts for generating a geoplot for the top 5 prediction of PlaNet network. It creates 6 plots in one image. First plot is a overview plot and the other plots are detailed plots for each prediction.
+
+The code runs in Matlab. All required files and folder structures are included here and should not be changed.
+
+### Prerequisites
+- Matlab
+- geoplot modul
+
+### Usage
+1. Copy the results of PlaNet for the Geoplots in the `Input` folder
+2. Fill in the imagenames in the array `Geoplots.m` and run the script
+3. You find the result image in the folder `Output`
